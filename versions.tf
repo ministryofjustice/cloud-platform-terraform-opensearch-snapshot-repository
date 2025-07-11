c7d49507040746f1cf92c244d005df98ff2290fa
Tim Cheung <152907271+timckt@users.noreply.github.com>
2025-07-11 11:47:21 +0100

    chore: update the variable and readme

--- versions.tf
+++ versions.tf
@@ -5,10 +5,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.0.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
-    }
     opensearch = {
       source  = "opensearch-project/opensearch"
       version = "2.3.1"
